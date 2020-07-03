24b842e162e47c5d41cf002b1e69166e9e3a81fc
pumacens <dancortes90@gmail.com>
2020-07-03 13:26:34 -0500

    Fixing date format in Colombian's news

--- src/app/tab6/tab6.module.ts
+++ src/app/tab6/tab6.module.ts
@@ -5,11 +5,11 @@ import { IonicModule } from '@ionic/angular';
 import { Tab6PageRoutingModule } from './tab6-routing.module';
 import { Tab6Page } from './tab6.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { environment } from '../../environments/environment';
-import { AngularFireAuthModule } from '@angular/fire/auth';
+// import { AngularFireModule } from '@angular/fire';
+// import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+// import { AngularFirestoreModule } from '@angular/fire/firestore';
+// import { environment } from '../../environments/environment';
+// import { AngularFireAuthModule } from '@angular/fire/auth';
 import { from } from 'rxjs';
 
 
@@ -20,10 +20,10 @@ import { from } from 'rxjs';
     IonicModule,
     Tab6PageRoutingModule,
     ExploreContainerComponentModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAnalyticsModule,
-    AngularFirestoreModule,
-    AngularFireAuthModule
+    // AngularFireModule.initializeApp(environment.firebase),
+    // AngularFireAnalyticsModule,
+    // AngularFirestoreModule,
+    // AngularFireAuthModule
   ],
   declarations: [Tab6Page]
 })
--- src/app/tab6/tab6.page.ts
+++ src/app/tab6/tab6.page.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/app';
-import { AlertController } from '@ionic/angular';
+// import { AngularFirestore } from '@angular/fire/firestore';
+// import { AngularFireAuth } from '@angular/fire/auth';
+// import { auth } from 'firebase/app';
+// import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab6',
@@ -11,52 +11,52 @@ import { AlertController } from '@ionic/angular';
 })
 export class Tab6Page {
 
-  users: any []= [];
-  currentUser: string = '';
-  loginEmail: string;
-  loginPassword: string;
+  // users: any []= [];
+  // currentUser: string = '';
+  // loginEmail: string;
+  // loginPassword: string;
 
-  constructor(private firestore: AngularFirestore,
-    public auth: AngularFireAuth,
-    public alertController: AlertController) { 
-  }
+  // constructor(private firestore: AngularFirestore,
+  //   public auth: AngularFireAuth,
+  //   public alertController: AlertController) { 
+  // }
 
-  ngOnInit(){
-    this.firestore.collection('users').valueChanges()
-    .subscribe((users)=>{
-      this.users = <any[]>users;
-    });
-  }
+  // ngOnInit(){
+  //   this.firestore.collection('users').valueChanges()
+  //   .subscribe((users)=>{
+  //     this.users = <any[]>users;
+  //   });
+  // }
 
   
-  login() {
-    this.auth.signInWithPopup(new auth.GoogleAuthProvider()).then((user)=>{
-      this.firestore.collection('users').doc(user.user.uid).set({email: user.user.email, displayname:user.user.displayName, method: user.user.providerId, })
-      this.ngOnInit();
-    });
-  }
+  // login() {
+  //   this.auth.signInWithPopup(new auth.GoogleAuthProvider()).then((user)=>{
+  //     this.firestore.collection('users').doc(user.user.uid).set({email: user.user.email, displayname:user.user.displayName, method: user.user.providerId, })
+  //     this.ngOnInit();
+  //   });
+  // }
 
-  loginEmailPassword(){
-    if(this.loginEmail !== undefined && this.loginEmail!== ''
-    && this.loginPassword !== undefined && this.loginPassword !== ''){
-      this.auth.signInWithEmailAndPassword(this.loginEmail,this.loginPassword).then(
-        (user)=>{
-          console.log(user);
-        }
-        /* (user)=>{
-        this.firestore.collection('users').doc(user.user.uid).set({email: user.user.email, displayname:user.user.displayName, method: user.user.providerId })
-        this.ngOnInit();
-        } */
-      ).catch((error)=>{
-        console.error(error);
-      });
-    } else{
+  // loginEmailPassword(){
+  //   if(this.loginEmail !== undefined && this.loginEmail!== ''
+  //   && this.loginPassword !== undefined && this.loginPassword !== ''){
+  //     this.auth.signInWithEmailAndPassword(this.loginEmail,this.loginPassword).then(
+  //       (user)=>{
+  //         console.log(user);
+  //       }
+  //       /* (user)=>{
+  //       this.firestore.collection('users').doc(user.user.uid).set({email: user.user.email, displayname:user.user.displayName, method: user.user.providerId })
+  //       this.ngOnInit();
+  //       } */
+  //     ).catch((error)=>{
+  //       console.error(error);
+  //     });
+  //   } else{
 
-    }
-  }
+  //   }
+  // }
 
-  logout() {
-    this.auth.signOut();
-  }
+  // logout() {
+  //   this.auth.signOut();
+  // }
 
 }
