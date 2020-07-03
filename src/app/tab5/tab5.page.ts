5a8c57d340a995359de2754cb307037072711deb
pumacens <dancortes90@gmail.com>
2020-07-03 12:47:19 -0500

    Fixing colombia news scrapping. Implementing Rss reader

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import * as xml2js from "xml2js";
+import { NewsRss } from '../news-rss';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-tab5',
@@ -11,16 +14,52 @@ export class Tab5Page {
   constructor(private http: HttpClient) {}
 
   private URL_oms = 'https://covid-noticias-oms.herokuapp.com/';
-  private URL_colombia = 'https://covid-noticias-colombia.herokuapp.com/';
+  // private URL_colombia = 'https://covid-noticias-colombia.herokuapp.com/';
+  // private URL_oms = 'https://www.who.int/feeds/entity/mediacentre/news/es/rss.xml';
+  private URL_colombia = 'https://www.minsalud.gov.co/_layouts/15/listfeed.aspx?List=%7BCC536310-0C15-4295-82EA-2FD45206219D%7D';
   private noticiasOmsActivadas = false;
-  private noticiasColombiaActivadas = false;
+  private noticiasColombiaActivadas = true;
+  private _descripcion;
+  // private str = '<div><b>Título:</b> Minsalud entrega recomendaciones de bioseguridad para el Día sin IVA</div> <div><b>Imagen de la página:</b> <img alt="" src="/fotos_carrusel_2020/ministro-020720.jpg" style="BORDER: 0px solid; "></div>';
+  // private test = $(this.str).find('img').attr('src');
   noticias_oms = [];
   noticias_colombia = [];
 
-
   getNoticias(){
-    this.http.get(this.URL_oms).subscribe((data: any[]) => {this.noticias_oms = data});
-    this.http.get(this.URL_colombia).subscribe((data: any[]) => {this.noticias_colombia = data});
+    this.http.get<any>(this.URL_oms, 
+                      { observe: "body"}).subscribe((data: any[]) => {this.noticias_oms = data});
+    // this.http.get<any>(this.URL_colombia, 
+    //                   { observe: "body"}).subscribe((data: any[]) => {this.noticias_colombia = data});
+
+    const requestOptions: Object = {
+      observe: "body",
+      responseType: "text"
+    };
+
+    this.http.
+          get<any>(this.URL_colombia, requestOptions).
+          subscribe((data) => {
+            let parseString = xml2js.parseString;
+            
+            parseString(data, (err, result: NewsRss) => {
+              result.rss.channel[0].item.forEach((noticia) => {
+                this._descripcion = $(noticia.description.toString());
+                
+                if (noticia.title.toString().indexOf('-') != -1) {
+                  this.noticias_colombia.push(
+                    {
+                      titulo: noticia.title.toString().replace(/-/g, ' '),
+                      fecha: noticia.pubDate,
+                      imagen: "https://www.minsalud.gov.co" + this._descripcion.find('img').attr('src'),
+                      link: "https://www.minsalud.gov.co/Paginas/" + noticia.title + ".aspx",
+                      parrafo: this._descripcion.find('em').text() || this._descripcion.find('span').text(),
+                    }
+                  )
+                }
+              })
+            });
+          });   
+
     console.log('noticias cargadas');
   }
 
@@ -45,3 +84,5 @@ export class Tab5Page {
     window.open(url_noticia), "_blank";
   }
 }
+
+export interface IRssData {}
