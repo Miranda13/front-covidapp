9ace42f0250d4d4e4744146c03dc6577ef59d618
pumacens <dancortes90@gmail.com>
2020-07-03 13:26:34 -0500

    Fixing date format in Colombian's news

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -14,22 +14,33 @@ export class Tab5Page {
   constructor(private http: HttpClient) {}
 
   private URL_oms = 'https://covid-noticias-oms.herokuapp.com/';
-  // private URL_colombia = 'https://covid-noticias-colombia.herokuapp.com/';
-  // private URL_oms = 'https://www.who.int/feeds/entity/mediacentre/news/es/rss.xml';
   private URL_colombia = 'https://www.minsalud.gov.co/_layouts/15/listfeed.aspx?List=%7BCC536310-0C15-4295-82EA-2FD45206219D%7D';
   private noticiasOmsActivadas = false;
   private noticiasColombiaActivadas = true;
   private _descripcion;
-  // private str = '<div><b>Título:</b> Minsalud entrega recomendaciones de bioseguridad para el Día sin IVA</div> <div><b>Imagen de la página:</b> <img alt="" src="/fotos_carrusel_2020/ministro-020720.jpg" style="BORDER: 0px solid; "></div>';
-  // private test = $(this.str).find('img').attr('src');
+  private _date;
   noticias_oms = [];
   noticias_colombia = [];
 
+  private meses = {
+    0: 'Enero',
+    1: 'Febrero',
+    2: 'Marzo',
+    3: 'Abril',
+    4: 'Mayo',
+    5: 'Junio',
+    6: 'Julio',
+    7: 'Agosto',
+    8: 'Septiembre',
+    9: 'Octubre',
+    10: 'Noviembre',
+    11: 'Diciembre'
+  }
+
   getNoticias(){
+
     this.http.get<any>(this.URL_oms, 
-                      { observe: "body"}).subscribe((data: any[]) => {this.noticias_oms = data});
-    // this.http.get<any>(this.URL_colombia, 
-    //                   { observe: "body"}).subscribe((data: any[]) => {this.noticias_colombia = data});
+      { observe: "body"}).subscribe((data: any[]) => {this.noticias_oms = data});
 
     const requestOptions: Object = {
       observe: "body",
@@ -40,16 +51,17 @@ export class Tab5Page {
           get<any>(this.URL_colombia, requestOptions).
           subscribe((data) => {
             let parseString = xml2js.parseString;
-            
+
             parseString(data, (err, result: NewsRss) => {
               result.rss.channel[0].item.forEach((noticia) => {
                 this._descripcion = $(noticia.description.toString());
-                
+                this._date = new Date(noticia.pubDate.toString());
+
                 if (noticia.title.toString().indexOf('-') != -1) {
                   this.noticias_colombia.push(
                     {
                       titulo: noticia.title.toString().replace(/-/g, ' '),
-                      fecha: noticia.pubDate,
+                      fecha: `${this._date.getDate()} de ${this.meses[this._date.getMonth()]} de ${this._date.getFullYear()}`,
                       imagen: "https://www.minsalud.gov.co" + this._descripcion.find('img').attr('src'),
                       link: "https://www.minsalud.gov.co/Paginas/" + noticia.title + ".aspx",
                       parrafo: this._descripcion.find('em').text() || this._descripcion.find('span').text(),
@@ -61,6 +73,7 @@ export class Tab5Page {
           });   
 
     console.log('noticias cargadas');
+
   }
 
 
