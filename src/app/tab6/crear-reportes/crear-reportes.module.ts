9a04a2cd53588e9b14e589416ca7ba59d60c9c9a
Miranda13Annie <anny13miranda@gmai.com>
2020-07-05 01:17:22 -0500

    add without function create reports

--- src/app/tab6/crear-reportes/crear-reportes.module.ts
+++ src/app/tab6/crear-reportes/crear-reportes.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { CrearReportesPageRoutingModule } from './crear-reportes-routing.module';
-
 import { CrearReportesPage } from './crear-reportes.page';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../../../environments/environment';
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CrearReportesPageRoutingModule
+    CrearReportesPageRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule
   ],
   declarations: [CrearReportesPage]
 })
