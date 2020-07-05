c5ae688a51ffa7798e007ad1c77a0d1593de6ec1
Miranda13Annie <anny13miranda@gmai.com>
2020-07-04 21:54:51 -0500

    add menu in principal

--- src/app/tab6/menu/menu-routing.module.ts
+++ src/app/tab6/menu/menu-routing.module.ts
@@ -6,7 +6,24 @@ import { MenuPage } from './menu.page';
 const routes: Routes = [
   {
     path: '',
-    component: MenuPage
+    component: MenuPage,
+    children: [
+      {
+        path: 'perfil',
+        //loadChildren: '../perfil/perfil.module'
+        loadChildren: () => import('../perfil/perfil.module').then(m => m.PerfilPageModule)
+      },
+      {
+        path: 'verReportes',
+        //loadChildren: '../ver-reportes/ver-reportes.module#verReportesModule'
+        loadChildren: () => import('../ver-reportes/ver-reportes.module').then(m => m.VerReportesPageModule)  
+      },
+      {
+        path: 'crearReportes',
+        loadChildren: () => import('../crear-reportes/crear-reportes.module').then(m => m.CrearReportesPageModule)
+      }
+
+    ]
   }
 ];
 
--- src/app/tab6/menu/menu.page.ts
+++ src/app/tab6/menu/menu.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,8 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.page.scss'],
 })
 export class MenuPage implements OnInit {
+  
+  pages =[
+    {
+      title: 'Mi perfil',
+      url: '/tabs/tab6/menu/perfil'
+    },
+    {
+      title: 'Ver reportes',
+      url: '/tabs/tab6/menu/verReportes'
+    },
+    {
+      title: 'Crear reporte',
+      url: '/tabs/tab6/menu/crearReportes'
+    }
+  ];
 
-  constructor() { }
+  selectedPath = '';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: RouterEvent)=>{
+      if (event && event.url){
+        this.selectedPath = event.url;
+      }
+    })
+   }
 
   ngOnInit() {
   }
