c217eaa90e9146ca145c3cbd156c2e0470af96ac
Miranda13Annie <anny13miranda@gmai.com>
2020-07-04 23:53:11 -0500

    add icons in menu

--- src/app/tab6/menu/menu.page.ts
+++ src/app/tab6/menu/menu.page.ts
@@ -10,23 +10,28 @@ import { auth } from 'firebase/app';
   styleUrls: ['./menu.page.scss'],
 })
 export class MenuPage implements OnInit {
-  
+
+    
   pages =[
     {
       title: 'Principal',
-      url: '/tabs/tab6/menu/principal'
+      url: '/tabs/tab6/menu/principal',
+      name: 'home-outline'
     },
     {
       title: 'Mi perfil',
-      url: '/tabs/tab6/menu/perfil'
+      url: '/tabs/tab6/menu/perfil',
+      name: 'person-circle-outline'
     },
     {
       title: 'Ver reportes',
-      url: '/tabs/tab6/menu/verReportes'
+      url: '/tabs/tab6/menu/verReportes',
+      name: 'document-text-outline'
     },
     {
       title: 'Crear reporte',
-      url: '/tabs/tab6/menu/crearReportes'
+      url: '/tabs/tab6/menu/crearReportes',
+      name: 'pencil-outline'
     }
   ];
 
@@ -43,6 +48,7 @@ export class MenuPage implements OnInit {
    }
 
   ngOnInit() {
+   
   }
 
   goToPrincipal(){
