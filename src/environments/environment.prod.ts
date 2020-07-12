e31e9c6f0a090ab5cb816c8d8740f899e3eb50cf
Miranda13Annie <miranda_anam@hotmail.com>
2020-07-12 09:38:11 -0500

    edit enviroment prod and fixed mistakes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBkXaf5LQO7BO2bRfWkXR6dfUukhUiFmuI",
+    authDomain: "covidapp-9a010.firebaseapp.com",
+    databaseURL: "https://covidapp-9a010.firebaseio.com",
+    projectId: "covidapp-9a010",
+    storageBucket: "covidapp-9a010.appspot.com",
+    messagingSenderId: "208851488079",
+    appId: "1:208851488079:web:f5f00de24954bdd177893a",
+    measurementId: "G-CR2ZHZ01SM"
+  }
 };
